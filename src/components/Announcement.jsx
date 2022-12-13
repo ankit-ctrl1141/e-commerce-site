a691acab08593ae348ec7e97cdabb23832ae0cd5
Ankit Kumar Verma <ankit.official025@gmail.com>
2022-12-13 23:13:53 +0530

    Announcement-section

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,21 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+   height: 30px;
+   background-color: rgb(0, 128, 128);
+   color: white;
+   display: flex;
+   align-items: center;
+   justify-content: center;
+   font-size: 14px;
+   font-weight: 500;
+`
+const Announcement = () => {
+    return (
+        <Container>
+            Super Deal! Free Shipping on Orders above ₹999
+        </Container>
+    )
+}
+
+export default Announcement
