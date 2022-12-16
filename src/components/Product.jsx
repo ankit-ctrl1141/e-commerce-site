3df3458bbe749c2cb9a93bd5a0f3981c4e3cf7bf
Ankit Kumar Verma <ankit.official025@gmail.com>
2022-12-16 15:51:57 +0530

    Styled_Components_hover_and_Animation

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,7 +3,22 @@ import React from 'react'
 import styled from 'styled-components'
 
 
-
+const InfoContainer = styled.div`
+    opacity: 0; //the icons will be hidden, they will only show up when hovered over any particular product (Conatiner).
+    width: 100%;
+    height: 100%;
+    position: absolute; //parent position : relative
+    top: 0; //property of position:absolute
+    left: 0; //property of position:absolute
+    background-color: rgb(0,0,0,.2);
+    z-index: 3;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: all .5s ease;
+    cursor: pointer;
+   
+`
 const Container = styled.div`
     flex : 1;  //as the parent Component i.e, Products Component has display:flex
     margin: 5px;
@@ -15,7 +30,10 @@ const Container = styled.div`
     background-color: #f3fbfd; 
     position: relative;
    
-    
+    &:hover ${InfoContainer}{ //we want that, this hover effect should get appllied on { InfoContainer Component } // so if want to do that hover effect from here then we should use this method of styled component.
+    /* We can even do this directly by using hover effect inside the InfoContainer itself. */
+    opacity: 1;
+}
     
 `
 const Circle = styled.div`
@@ -25,26 +43,9 @@ const Circle = styled.div`
     background-color: white;
     position: absolute; //to fix the position of circle (as we are not getting the proper shape of the circle if the position is not set)
 `
-
 const Image = styled.img`
      height: 75%;
      z-index: 2;   // so the circle does not come above the image
-`
-
-const InfoContainer = styled.div`
-    
-    width: 100%;
-    height: 100%;
-    position: absolute; //parent position : relative
-    top: 0; //property of position:absolute
-    left: 0; //property of position:absolute
-    background-color: rgb(0,0,0,.2);
-    z-index: 3;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    
-   
 `
 
 const Icon = styled.div`
