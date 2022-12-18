6f3fb73403cad05542d926757f899a11f9c644c9
Ankit Kumar Verma <ankit.official025@gmail.com>
2022-12-19 02:55:03 +0530

    Login/sign_page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 //import Home from "./pages/Home";
 // import Product from "./pages/Product";
-import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
-    <Register/>
+    <Login/>
   );
 }
 
