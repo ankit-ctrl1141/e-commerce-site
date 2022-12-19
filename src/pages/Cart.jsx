bddd744d3434cb8ed7f7466f011bbd4f84bf91c8
Ankit Kumar Verma <ankit.official025@gmail.com>
2022-12-19 15:29:06 +0530

    finished_shopping_cart_design

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -109,7 +109,35 @@ const Hr = styled.hr`
 `
 const Summary = styled.div`
     flex: 1;
+    border: 0.5px solid lightgray;
+    border-radius: 10px;
+    padding: 20px;
+    height: 55vh;
     
+`
+const SummaryTitle = styled.h1`
+    font-weight: 200;
+`
+const SummaryItem = styled.div`
+    margin: 30px 0px;
+    display: flex;
+    justify-content: space-between;
+    font-weight: ${(props)=> props.type === "total" && "500"};
+    font-size: ${(props) => props.type === "total" && "24px"};
+`
+const SummaryItemText = styled.span`
+
+`
+const SummaryItemPrice = styled.span`
+    
+`
+const SummaryButton = styled.button`
+    width: 100%;
+    border: none;
+    padding: 10px;
+    background-color: teal;
+    color: white;
+    font-weight: 500;
 `
 
 
@@ -174,7 +202,26 @@ const Cart = () => {
                        </PriceDetail>
                     </Product> 
                  </Info>
-                 <Summary>summary</Summary>
+                 <Summary>
+                    <SummaryTitle>ORDER SUMMARY</SummaryTitle>
+                    <SummaryItem>
+                       <SummaryItemText>Subtotal</SummaryItemText>
+                       <SummaryItemPrice>₹ 3478</SummaryItemPrice>
+                    </SummaryItem>
+                    <SummaryItem>
+                       <SummaryItemText>Estimated Shipping</SummaryItemText>
+                       <SummaryItemPrice>₹ 75</SummaryItemPrice>
+                    </SummaryItem>
+                    <SummaryItem>
+                       <SummaryItemText>Shipping Discount</SummaryItemText>
+                       <SummaryItemPrice>₹ -75</SummaryItemPrice>
+                    </SummaryItem>
+                    <SummaryItem type="total">
+                       <SummaryItemText>Total</SummaryItemText>
+                       <SummaryItemPrice>₹ 3478</SummaryItemPrice>
+                    </SummaryItem>
+                   <SummaryButton>CHECKOUT NOW</SummaryButton>
+                 </Summary>
               </Bottom>
           </Wrapper>
        <Footer/>
