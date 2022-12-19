15d5b66af924573fc06e44fb5da1301a2d359bad
Ankit Kumar Verma <ankit.official025@gmail.com>
2022-12-19 11:03:51 +0530

    product-detail_in_cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 //import Home from "./pages/Home";
 // import Product from "./pages/Product";
-import Login from "./pages/Login";
+import Cart from "./pages/Cart";
 
 const App = () => {
   return (
-    <Login/>
+    <Cart/>
   );
 }
 
